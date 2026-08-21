21679d3cd21d61e63fa0314bead45b71506286f6
RawanAhmed00 <ahmdrwan577@gmail.com>
2026-08-21 12:29:10 +0300

    Update API base URL to local development for all front-end environments

--- case1-front-end/admin-frontend/assets/js/api/api.js
+++ case1-front-end/admin-frontend/assets/js/api/api.js
@@ -23,7 +23,7 @@
      * =================================================================== */
 
     const API_CONFIG = {
-    baseUrl: "https://tailora-production-d349.up.railway.app/api",//"http://127.0.0.1:8000/api"
+    baseUrl:"http://127.0.0.1:8000/api", //"https://tailora-production-d349.up.railway.app/api",
     tokenKey: "tailora_token"
 };
 
